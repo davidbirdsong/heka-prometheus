7bce880563bebe08a96c7738b363a6cb538c5cd9
david birdsong <david.birdsong@imgix.com>
2015-05-31 21:58:46 -0700

    initial, not working yet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # heka-promethus
-output plugin which exposes an endpoint for prometheus to scrape
+[mozilla heka](https://github.com/mozilla-services/heka) output plugin which exposes an endpoint for prometheus to scrape
+
+### Usage
+TODO
---
+++ prometheus.go
@@ -0,0 +1,119 @@
+package prometheus
+
+import (
+	"errors"
+	"github.com/mozilla-services/heka/message"
+	"github.com/mozilla-services/heka/pipeline"
+	"github.com/prometheus/client_golang/prometheus"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+)
+
+type PromOutConfig struct {
+	Address     string
+	MetricsPath string
+}
+
+type PromOut struct {
+	config    *PromOutConfig
+	rlock     *sync.RWMutex
+	gaugeVecs map[string]*prometheus.GaugeVec
+	gauges    map[string]prometheus.Gauge
+}
+
+func (p *PromOut) ConfigStruct() interface{} {
+	return &PromOutConfig{
+		Address: "0.0.0.0:9107",
+	}
+}
+
+func (p *PromOut) Init(config interface{}) error {
+	p.config = config.(*PromOutConfig)
+	p.rlock = &sync.RWMutex{}
+	return nil
+}
+
+func (p *PromOut) validMsg(m *message.Message) bool {
+	/*
+		switch string.ToLowerm.GetType()) {
+			case
+		}
+	*/
+	return true
+}
+
+func (p *PromOut) Describe(ch chan<- *prometheus.Desc) {
+
+	p.rlock.RLock()
+	defer p.rlock.RUnlock()
+
+	for _, gauge := range p.gauges {
+		c <- gauge.Desc()
+	}
+	for _, counter := range p.counters {
+		c <- counter.Desc()
+	}
+	for _, gaugeVec := range p.gaugeVecs {
+		gaugeVec.Describe(c)
+	}
+
+	for _, counterVec := range p.counterVecs {
+		counterVec.Describe(c)
+	}
+}
+func (p *PromOut) Collect(ch chan<- prometheus.Metric) {
+	p.rlock.Lock()
+	defer p.rlock.Unlock()
+
+}
+
+func (p *PromOut) Run(or pipeline.OutputRunner, h pipeline.PluginHelper) (err error) {
+	var (
+		pack         *pipeline.PipelinePack
+		field        *message.Field
+		seen_metrics map[string]map[string]bool
+	)
+
+	for pack = range or.InChan {
+		if p.validMsg(pack.Message) {
+			namespace := pack.Message
+			msgType := strings.ToLower(pack.Message.GetType())
+			switch strings.ToLower(pack.Message.GetType()) {
+			case "gaugevecs":
+			case "gauge":
+
+			}
+		}
+		pack.Recycle()
+	}
+
+}
+func gaugeOpts(m *message.Message) prometheus.GaugeOpts {
+	return prometheus.GaugeOpts{
+		Namespace: m.FindFirstField("Namespace"),
+		Name:      m.FindFirstField("Name"),
+		Help:      m.FindFirstField("Help"),
+	}
+
+}
+
+func newGaugeVec(m *message.Message, opts prometheus.GaugeOpts) (*prometheus.GaugeVec, error) {
+
+	return prometheus.NewGaugeVec(gaugeOpts(m),
+		m.FindFirstField("labels"),
+	)
+
+}
+func newGauge(m *message.Message) (prometheus.Gauge, error) {
+}
+
+func init() {
+	pipeline.RegisterPlugin("PrometheusOutput", func() interface{} {
+		return new(PromOut)
+	})
+}
