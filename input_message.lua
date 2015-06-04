530ebcad2b1c360c1316678400761d356e0d1ca6
david birdsong <david.birdsong@imgix.com>
2015-06-04 02:39:56 -0700

    working now

--- input_message.lua
+++ input_message.lua
@@ -1,17 +1,17 @@
 {
 	Logger = "prometheus_data", -- only matters for message_matcher
 	Type = "prom_translated", -- only matters for message_matcher
-	Payload = "", -- no used
+	Payload = "", -- not examined 
 	Fields = {
-		metric_type = 'GaugeVec', -- required
+		metric_type = 'Gauge', -- required
 		metric_name = 'net_counters', -- required
+		metric_help = 'number of packets on network', -- required
 
 		-- required, must be value_type=3 
 		-- see https://github.com/mozilla-services/heka/blob/dev/message/message.proto#L23
 		metric_value = {value_type=3, value=100}, 
 
-		Help = "number of packets on network", -- optional
-		Help = "number of packets on network", -- optional
+		metric_expires = '70s',  -- optional, golang duration strings '100ms' etc..
 
 		-- optional, but table lengths must match each other
 		-- type is inferred by heka, but must be string
--- prometheus.go
+++ prometheus.go
@@ -20,26 +20,26 @@ import (
 )
 
 var (
-	metricFieldVal         = "metric_value"
-	metricFieldName        = "metric_name"
-	metricFieldType        = "metric_type"
-	metricFieldHelp        = "metric_help"
-	metricFieldLabelnames  = "metric_labelnames"
-	metricFieldLabelvalues = "metric_labelvalues"
+	fieldVal         = "metric_value"
+	fieldName        = "metric_name"
+	fieldType        = "metric_type"
+	fieldHelp        = "metric_help"
+	fieldExpires     = "metric_expires"
+	fieldLabelnames  = "metric_labelnames"
+	fieldLabelvalues = "metric_labelvalues"
 )
 
 type hekaSample struct {
 	Value      float64
 	Labels     map[string]string
-	Expires    time.Time
 	Name, Help string
-	Type       prometheus.ValueType
+	Expires    time.Time
 	desc       *prometheus.Desc
+	Type       prometheus.ValueType
 }
 
-func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, error) {
+func newHekaSample(m *message.Message, defaultTTL time.Duration) (*hekaSample, error) {
 	h := new(hekaSample)
-
 	returnEnforceSingleString := func(n string, required bool) (string, error) {
 
 		if f := m.FindFirstField(n); f != nil {
@@ -61,7 +61,7 @@ func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, er
 		mtype string
 		err   error
 	)
-	mtype, err = returnEnforceSingleString(metricFieldType, true)
+	mtype, err = returnEnforceSingleString(fieldType, true)
 	if err != nil {
 		return nil, err
 	}
@@ -74,13 +74,16 @@ func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, er
 		h.Type = prometheus.UntypedValue
 	}
 
-	h.Name, err = returnEnforceSingleString(metricFieldName, true)
+	h.Name, err = returnEnforceSingleString(fieldName, true)
 	if err != nil {
 		return nil, err
 	}
-	h.Help, _ = returnEnforceSingleString("Help", false)
+	h.Help, err = returnEnforceSingleString(fieldHelp, true)
+	if err != nil {
+		return nil, err
+	}
 
-	if f := m.FindFirstField("Expires"); f != nil && len(f.GetValueInteger()) > 0 {
+	if f := m.FindFirstField(fieldExpires); f != nil && len(f.GetValueInteger()) > 0 {
 		h.Expires = time.Unix(0, f.GetValueInteger()[0])
 
 	} else {
@@ -88,16 +91,16 @@ func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, er
 		h.Expires = time.Unix(0, m.GetTimestamp()).Add(defaultTTL)
 	}
 
-	if f := m.FindFirstField(metricFieldVal); f != nil && len(f.GetValueDouble()) == 1 {
+	if f := m.FindFirstField(fieldVal); f != nil && len(f.GetValueDouble()) == 1 {
 		h.Value = f.GetValueDouble()[0]
 
 	} else {
-		return nil, fmt.Errorf("single, required field invalid: %s", metricFieldVal)
+		return nil, fmt.Errorf("single, required field invalid: %s", fieldVal)
 
 	}
 
-	lNameField := m.FindFirstField(metricFieldLabelnames)
-	lValField := m.FindFirstField(metricFieldLabelvalues)
+	lNameField := m.FindFirstField(fieldLabelnames)
+	lValField := m.FindFirstField(fieldLabelvalues)
 	if lNameField != nil {
 		if lValField == nil {
 			return nil, fmt.Errorf("missing label values for label names field")
@@ -116,7 +119,6 @@ func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, er
 		for i, n := range names {
 			h.Labels[n] = vals[i]
 		}
-
 	}
 
 	h.desc = prometheus.NewDesc(h.Name, h.Help, []string{}, h.Labels)
@@ -127,7 +129,7 @@ func newHekaSampe(m *message.Message, defaultTTL time.Duration) (*hekaSample, er
 
 type PromOutConfig struct {
 	Address    string
-	DefaultTTL time.Duration
+	DefaultTTL string
 }
 
 type PromOut struct {
@@ -136,14 +138,16 @@ type PromOut struct {
 	rlock   *sync.RWMutex
 	samples map[string]*hekaSample
 
-	inSuccess prometheus.Counter
-	inFailure prometheus.Counter
-	errLogger func(error)
+	inSuccess       prometheus.Counter
+	inFailure       prometheus.Counter
+	errLogger       func(error)
+	defaultDuration time.Duration
 }
 
 func (p *PromOut) ConfigStruct() interface{} {
 	return &PromOutConfig{
-		Address: "0.0.0.0:9107",
+		Address:    "0.0.0.0:9107",
+		DefaultTTL: "90s",
 	}
 }
 
@@ -164,6 +168,12 @@ func (p *PromOut) Init(config interface{}) error {
 	)
 
 	p.config = config.(*PromOutConfig)
+
+	var err error
+	p.defaultDuration, err = time.ParseDuration(p.config.DefaultTTL)
+	if err != nil {
+		return err
+	}
 	p.rlock = &sync.RWMutex{}
 
 	e := prometheus.Register(p)
@@ -185,15 +195,12 @@ func (p *PromOut) Describe(ch chan<- *prometheus.Desc) {
 
 }
 func (p *PromOut) Collect(ch chan<- prometheus.Metric) {
-	p.rlock.Lock()
-	defer p.rlock.Unlock()
-
 	ch <- p.inSuccess
 	ch <- p.inFailure
 
 	samples := make([]*hekaSample, 0, len(p.samples))
 	p.rlock.RLock()
-	for _, s := range samples {
+	for _, s := range p.samples {
 		samples = append(samples, s)
 	}
 	p.rlock.RUnlock()
@@ -203,7 +210,6 @@ func (p *PromOut) Collect(ch chan<- prometheus.Metric) {
 		if now.After(s.Expires) {
 			continue
 		}
-
 		m, err := prometheus.NewConstMetric(s.desc, s.Type, s.Value)
 		if err != nil {
 
@@ -227,16 +233,21 @@ func (p *PromOut) Run(or pipeline.OutputRunner, ph pipeline.PluginHelper) (err e
 	for running {
 		select {
 		case pack, running = <-or.InChan():
-			h, err = newHekaSampe(pack.Message, p.config.DefaultTTL)
+			if !running {
+				continue
+			}
+			h, err = newHekaSample(pack.Message, p.defaultDuration)
 			if err == nil {
 
+				or.LogMessage(h.desc.String())
 				p.rlock.Lock()
 				p.samples[h.desc.String()] = h
 				p.rlock.Unlock()
 				p.inSuccess.Inc()
 
 			} else {
-				or.LogError(err)
+				b, _ := or.Encode(pack)
+				or.LogError(fmt.Errorf("%v message\n<msg>\n%s\n</msg>", err, b))
 				p.inFailure.Inc()
 			}
 
